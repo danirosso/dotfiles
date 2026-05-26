3a42ee497df14911d5e4978fa752c00c813f467d
rosso <daniel.7463138@edu.univali.br>
2026-05-26 19:08:43 -0300

    add capslock notification and clean up dunstrc

--- .name.sh
+++ .name.sh
@@ -2,9 +2,15 @@
 while true; do
 	getvolume=$(wpctl 2>/dev/null get-volume @DEFAULT_SINK@| awk ' {print $2 * 100} ')
 	getdate=$(date +' %d/%m/%y  %R')
+	capstate=$(xset q | awk '/Caps Lock/ {print $4}')
 
 	name="[$getvolume] $getdate"
 
 	xsetroot -name " $name"
+
+	if [ "$capstate" == "on" ]; then
+		notify-send -a "capslock" "CAPS LOCK ON"
+	fi
+
 	sleep 1
 done
